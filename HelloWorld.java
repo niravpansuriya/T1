8ad09b2689945e4ce0a74c11e41ccd56743faf36
niravpansuriya <pniravc36@gmail.com>
2024-09-17 19:24:07 -0400

    new changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"`);
+        System.out.println("Hello, World!");
+
+
+        System.out.println("Hey How are you?");
     }
 }
